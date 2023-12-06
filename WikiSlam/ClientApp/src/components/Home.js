4e1b1c207fcf236f08fc0124930f0fea202e2003
Hevano <evanohirabe@gmail.com>
2023-12-05 21:33:52 -0800

    Added logic to rejoin from lobby

--- WikiSlam/ClientApp/src/components/Home.js
+++ WikiSlam/ClientApp/src/components/Home.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Button, Col, Container, Row, Accordion, Stack } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
@@ -16,6 +16,26 @@ export function Home() {
   const [showCreateLobby, setShowCreateLobby] = useState(false)
   const [showJoin, setShowJoin] = useState(false)
   const [createLobbyLoading, setCreateLobbyLoading] = useState(false)
+  const [canRejoin, setCanRejoin] = useState(false)
+
+  //Checks if we can rejoin a game in progress
+  useEffect(()=>{
+    let oldLobby = JSON.parse(localStorage.getItem('wikislam-lobby'))
+    let oldUser = JSON.parse(localStorage.getItem('wikislam-user'))
+
+    let clearStateOnError = err => {
+      localStorage.clearItem('wikislam-lobby')
+      localStorage.clearItem('wikislam-user')
+    };
+
+    if(oldLobby && oldUser){
+      axios.get(`api/lobby/${oldLobby.id}`).then((res)=>{
+        axios.get(`api/lobby/${oldLobby.id}`).then((res)=>{
+          setCanRejoin(true);
+        }).catch(clearStateOnError)
+      }).catch(clearStateOnError)
+    }
+  }, [])
 
   function createLobby(userName){
     setCreateLobbyLoading(true)
@@ -29,6 +49,18 @@ export function Home() {
     })
   }
 
+  function rejoin(){
+    let oldLobby = JSON.parse(localStorage.getItem('wikislam-lobby'))
+    let oldUser = JSON.parse(localStorage.getItem('wikislam-user'))
+    let duration = oldLobby.roundDuration.split(":")
+    duration = (parseInt(duration[0]) * 3600) + (parseInt(duration[1]) * 60) + (parseInt(duration[2]))
+    let startTime = oldLobby.roundStartTimestamp.split(":")
+    startTime = (parseInt(startTime[0]) * 3600) + (parseInt(startTime[1]) * 60) + (parseInt(startTime[2]))
+    
+    localStorage.setItem('wikislam-gamestate', (Date.now() + duration > startTime) ? "Lobby" : "Game")
+    navigate("/lobby", {state:{lobby: oldLobby, user: oldUser}});
+  }
+
   return (
     <>
     <EditNameModal show={showCreateLobby} handleClose={()=>{setShowCreateLobby(false)}} handleCreate={createLobby} isLoading={createLobbyLoading}/>
@@ -38,6 +70,7 @@ export function Home() {
       <motion.div className='d-inline-block' animate={{x:[-1000, 0]}} transition={{ ease: "easeOut", duration: 0.2 }}>WIKI</motion.div>
       <motion.div className='d-inline-block' animate={{x:[1000, 0]}}transition={{ ease: "easeOut", duration: 0.2 }}>SLAM!</motion.div>
     </h1>
+    {canRejoin && <motion.div className="mt-auto w-50" whileHover={{scale: 1.2}}><Button className='fs-2 w-100' onClick={rejoin}>Rejoin</Button></motion.div>}
     <motion.div className="mt-auto w-50" whileHover={{scale: 1.2}}><Button className='fs-2 w-100' onClick={()=>{setShowJoin(true)}}>Play</Button></motion.div>
     <motion.div className="mx-auto m-5 w-25" whileHover={{scale: 1.1}}><Button className='fs-5 w-100' onClick={()=>{setShowCreateLobby(true)}}>Create Lobby</Button></motion.div>
     </div>
