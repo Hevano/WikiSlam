8ce4dc0bce5e90c81209c0edb3c778652c63acc2
Hevano <evanohirabe@gmail.com>
2024-01-12 17:51:00 -0800

    improved responsiveness of layout

--- WikiSlam/ClientApp/src/components/HowToPlay.js
+++ WikiSlam/ClientApp/src/components/HowToPlay.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Stack } from 'react-bootstrap'
+import { Card, Stack, Container, Row, Col } from 'react-bootstrap'
 import InspectImage from "../assets/Wikislam-inspect.webp";
 import RankImage from "../assets/Wikislam-rank.webp";
 import RollImage from "../assets/Wikislam-roll.webp";
@@ -8,7 +8,8 @@ export default function HowToPlay() {
   return (
     <Stack gap={4}>
     <Card className='w-100 h-auto'>
-      <Stack direction="horizontal">
+      <Container fluid><Row>
+      <Col md={8} sm={12}>
       <Card.Body>
         <Card.Title className="fs-2">Roll For A Wikipedia article</Card.Title>
         <Card.Text>
@@ -19,12 +20,18 @@ export default function HowToPlay() {
           <span className='text-body-secondary'> Pro tip: spending some time to roll for an article with high base stats will improve your chances of winning.</span>
         </Card.Text>
       </Card.Body>
-      <Card.Img className='m-3' style={{height: "16rem", width: "auto"}} variant="top" src={RollImage} />
-      </Stack>
+      </Col>
+      <Col md={2} sm={6}>
+      <Card.Img className='img-fluid'  variant="top" src={RollImage} />
+      </Col>
+      </Row></Container>
     </Card>
     <Card className='w-100 h-auto'>
-      <Stack direction="horizontal">
-      <Card.Img className='m-3' style={{height: "16rem", width: "auto"}} variant="top" src={InspectImage} />
+      <Container fluid><Row>
+      <Col md={2} sm={6}>
+      <Card.Img className='img-fluid' variant="top" src={InspectImage} />
+      </Col>
+      <Col md={8} sm={12}>
       <Card.Body>
         <Card.Title className="fs-2">Identify The Quotes</Card.Title>
         <Card.Text>
@@ -33,21 +40,28 @@ export default function HowToPlay() {
           Picking the quote from your article will <span className='text-info'>LEVEL UP</span> your article, but guessing wrong will cause your article to <span className='text-danger'>LEVEL DOWN</span>.
         </Card.Text>
       </Card.Body>
-      </Stack>
+      </Col>
+      </Row></Container>
     </Card>
     <Card className='w-100 h-auto'>
-      <Stack direction="horizontal">
-      <Card.Body>
-        <Card.Title className="fs-2">Outrank Your Opponents</Card.Title>
-        <Card.Text>
-          Your article's strength, dexterity, and willpower increase when it levels up. 
-          At the end of the 2 minute round, everyone's articles are ranked based on these stats.
-          The person with the highest ranked article is declared the  <span className='text-success'>WINNER</span>.
-        </Card.Text>
-      </Card.Body>
-      <Card.Img style={{height: "16rem", width: "auto"}} variant="top" src={RankImage} />
-      </Stack>
+      <Container fluid><Row>
+      <Col md={8} sm={12}>
+        <Card.Body>
+          <Card.Title className="fs-2">Outrank Your Opponents</Card.Title>
+          <Card.Text>
+            Your article's strength, dexterity, and willpower increase when it levels up. 
+            At the end of the 2 minute round, everyone's articles are ranked based on these stats.
+            The person with the highest ranked article is declared the  <span className='text-success'>WINNER</span>.
+          </Card.Text>
+        </Card.Body>
+      </Col>
+      <Col md={2} sm={2}>
+        <Card.Img className='img-fluid' src={RankImage} />
+      </Col>
+      </Row></Container>
     </Card>
     </Stack>
   )
 }
+
+
